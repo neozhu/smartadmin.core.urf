d9186067a039a3064a2cf896d969f1ce1054baac
neozhu <new163@163.com>
2020-06-16 20:33:32 +0800

    xxx

---
+++ smartadmin-core-urf/src/SmartAdmin.Dto/UserInRolesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartAdmin.Dto
+{
+  public class UserInRolesViewModel
+  {
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public string GivenName { get; set; }
+    public string[] Roles { get; set; }
+  }
+}
---
+++ smartadmin-core-urf/src/SmartAdmin.WebUI/Controllers/RoleManageController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartAdmin.Dto;
+using SmartAdmin.WebUI.Data.Models;
+
+namespace SmartAdmin.WebUI.Controllers
+{
+  public class RoleManageController : Controller
+  {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    public RoleManageController(UserManager<ApplicationUser> userManager,
+      RoleManager<IdentityRole> roleManager
+      )
+    {
+      _userManager = userManager;
+      _roleManager = roleManager;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+      var roles = await _roleManager.Roles.OrderBy(x=>x.Name).ToListAsync();
+      var users = await _userManager.Users.OrderBy(x => x.UserName).ToListAsync();
+      var userinroles = new List<UserInRolesViewModel>();
+      foreach (var user in users) {
+        var inroles = await _userManager.GetRolesAsync(user);
+        userinroles.Add(new UserInRolesViewModel()
+        {
+           GivenName=user.GivenName,
+            Roles=inroles.ToArray(),
+             UserId=user.Id,
+              UserName=user.UserName
+        });
+      }
+      ViewBag.roles = roles;
+      ViewBag.userinroles = userinroles;
+
+      return View();
+    }
+  }
+}
