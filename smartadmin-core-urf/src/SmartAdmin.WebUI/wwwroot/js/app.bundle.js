01cab7a023625568431435f9c47635890eae3513
neozhu <new163@163.com>
2020-07-17 14:36:29 +0800

    汉化日期

--- smartadmin-core-urf/src/SmartAdmin.WebUI/wwwroot/js/app.bundle.js
+++ smartadmin-core-urf/src/SmartAdmin.WebUI/wwwroot/js/app.bundle.js
@@ -1224,8 +1224,8 @@ var initApp = (function(app) {
 		
 		/* Add app date to js-get-date */
 		if ( myapp_config.appDateHook.length ) {
-			var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-				day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      var months = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+        day = ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
 				now = new Date(),
 				formatted = day[now.getDay()] + ', ' +  
 							months[now.getMonth()] + ' ' +  
