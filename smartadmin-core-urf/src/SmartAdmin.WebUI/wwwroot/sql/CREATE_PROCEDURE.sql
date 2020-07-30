3afce4d37ff98f508f6041c882359e12578610e6
neozhu <new163@163.com>
2020-07-30 09:02:46 +0800

    添加自定义递增序列表存储过程

---
+++ smartadmin-core-urf/src/SmartAdmin.WebUI/wwwroot/sql/CREATE_PROCEDURE.sql
@@ -0,0 +1,37 @@
+﻿USE [smartdb]
+GO
+
+/****** Object:  StoredProcedure [dbo].[SP_NextVal]    Script Date: 2020/7/30 8:59:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[SP_NextVal](@prefix nvarchar(10))
+as
+begin  
+declare @val int=1;
+IF NOT EXISTS (SELECT * FROM [dbo].[Sequence] WHERE prefix = @prefix)
+ begin
+  begin try
+      INSERT INTO [dbo].[Sequence]
+        (prefix, seed)
+      VALUES (@prefix, @val);
+      select @val
+  end try
+  begin catch
+    update [dbo].[Sequence] set seed=seed+1,@val=seed+1 where prefix = @prefix
+    select @val
+  end catch
+ end
+else
+ begin
+ --WAITFOR DELAY '00:00:10.000';
+ update [dbo].[Sequence] set seed=seed+1,@val=seed+1 where prefix = @prefix
+ select @val
+ end
+end 
+GO
+
+
---
+++ smartadmin-core-urf/src/SmartAdmin.WebUI/wwwroot/sql/CREATE_TABLE.sql
@@ -0,0 +1,22 @@
+﻿USE [smartdb]
+GO
+
+/****** Object:  Table [dbo].[Sequence] 序列种子表   Script Date: 2020/7/30 8:57:11 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Sequence](
+	[prefix] [nvarchar](10) NOT NULL,
+	[seed] [int] NULL,
+ CONSTRAINT [PK_Sequence] PRIMARY KEY CLUSTERED 
+(
+	[prefix] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Sequence] ADD  CONSTRAINT [DF_Sequence_seed]  DEFAULT ((0)) FOR [seed]
+GO
